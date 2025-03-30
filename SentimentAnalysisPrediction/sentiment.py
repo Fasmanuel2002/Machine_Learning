48cc8db6dcc83973daf587a6b51981490ef2b1d3
Manuel Ruiz <manuelfasruiz@gmail.com>
2025-03-30 20:35:14 +0200

    New Project

---
+++ SentimentAnalysisPrediction/sentiment.py
@@ -0,0 +1,15 @@
+import pandas
+import numpy
+import keras
+from sklearn.model_selection import train_test_split
+
+def main():
+    ...
+    
+    
+def data_split(df):
+    ...
+    
+def get_model():    
+    ...
+

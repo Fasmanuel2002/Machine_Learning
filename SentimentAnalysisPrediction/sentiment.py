fd963c6863b99339869cc422c55c0a805b4c2d81
Manuel Ruiz <manuelfasruiz@gmail.com>
2025-04-02 13:17:34 +0200

    everything

--- SentimentAnalysisPrediction/sentiment.py
+++ SentimentAnalysisPrediction/sentiment.py
@@ -1,15 +1,44 @@
-import pandas
-import numpy
-import keras
-from sklearn.model_selection import train_test_split
+import nltk 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+plt.style.use('ggplot')
+from nltk.sentiment import SentimentIntensityAnalyzer
+from tqdm.notebook import tqdm
 
 def main():
-    ...
+    df = pd.read_csv("SentimentAnalysis.csv")
+    example = df['statement'][50659]
     
-    
-def data_split(df):
-    ...
-    
-def get_model():    
-    ...
+    """   
+    tokens = nltk.word_tokenize(example)
+    print(tokens[:10])
 
+
+    tagged = nltk.pos_tag(tokens)
+    print(tagged[:10])
+    
+    
+    entities = nltk.chunk.ne_chunk(tagged)
+    entities.pprint()
+    """
+    
+    sia = SentimentIntensityAnalyzer()
+    print(sia.polarity_scores(example))
+    print(example)
+    
+    
+    
+    
+    res = {}
+    for i , row in tqdm(df.iterrows(),total=len(df)) :
+        opinions = row['statement']
+        perId = row['id']
+        res[perId]= sia.polarity_scores(opinions)
+    
+    
+    print(res)
+    
+
+if __name__ == '__main__':
+    main()

bf754c1f3ceda033b7e76525dceddc05ed6339be
Manuel Ruiz <manuelfasruiz@gmail.com>
2025-04-02 22:13:42 +0200

    working

--- SentimentAnalysis(nltk)/sentiment.py
+++ SentimentAnalysis(nltk)/sentiment.py
@@ -1,44 +1,100 @@
-import nltk 
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-plt.style.use('ggplot')
+import numpy as np
+import tqdm
 from nltk.sentiment import SentimentIntensityAnalyzer
-from tqdm.notebook import tqdm
 
 def main():
-    df = pd.read_csv("SentimentAnalysis.csv")
-    example = df['statement'][50659]
+    df = pd.read_csv('SentimentAnalisisNew.csv')
     
-    """   
-    tokens = nltk.word_tokenize(example)
-    print(tokens[:10])
+    X = df.iloc[:, 2:6].values
+    Y = df['status']
+    
+    status_map = {"Anxiety":[1,0,0,0,0,0,0], "Normal":[0,1,0,0,0,0,0], "Suicidal":[0,0,1,0,0,0,0],
+                  "Depression":[0,0,0,1,0,0,0], "Stress":[0,0,0,0,1,0,0],
+                  "Bi-Polar":[0,0,0,0,0,1,0], "Personality Disorder":[0,0,0,0,0,0,1]}
+    Y_status_Map = pd.DataFrame(Y.map(status_map).tolist(), columns=["Anxiety","Normal","Suicidal","Depression","Stress","Bi-Polar","Personality Disorder"]).values()
 
 
-    tagged = nltk.pos_tag(tokens)
-    print(tagged[:10])
+## 1)Layers
+def layer(X,Y_status_Map):
+    n_x = X.shape[1]
+    n_y = Y_status_Map.shape[1]
+
+    return n_x, n_y
+
+
+##2)Parameters Initialization
+def parameters_initialization(n_x,n_y):
+    W = np.random.rand(n_x,n_y)
+    b = np.zeros((n_y,1))
+
     
-    
-    entities = nltk.chunk.ne_chunk(tagged)
-    entities.pprint()
-    """
-    
-    sia = SentimentIntensityAnalyzer()
-    print(sia.polarity_scores(example))
-    print(example)
-    
-    
-    
-    
-    res = {}
-    for i , row in tqdm(df.iterrows(),total=len(df)) :
-        opinions = row['statement']
-        perId = row['id']
-        res[perId]= sia.polarity_scores(opinions)
-    
-    
-    print(res)
+    parameters = {
+        "W":W,
+        "b":b
+    }
     
 
-if __name__ == '__main__':
+    return parameters
+
+## 3) Activation "SoftMax"
+
+def activation_softMax(Z):
+    expZ = np.exp(Z - np.max(Z, axis=1, keepdims=True))
+    return expZ / np.sum(expZ, axis=1, keepdims=True)
+    
+## 4) Forward Propagation
+def forward_propagation(X,parameters):
+    W = parameters['W']
+    b = parameters['b']
+    
+    Z = np.matmul(X, W) + b.T
+    Z_softMax = activation_softMax(Z)
+
+    return Z_softMax
+
+## 5) Log Loss function in SoftMax Likehood(Maximize)
+
+# y = Y_status_Map
+# h(x) = ^y 
+def cost_fuction(Y_status_Map,Z_softMax):
+
+    m = Y_status_Map.shape[0]
+    cost_fuctionSoftMax = -np.sum(Y_status_Map * np.log(Z_softMax)) / m
+    return cost_fuctionSoftMax
+
+## 6) Back propagation
+def backPropagation(Z_softMax,X, Y_status_Map, iterations):
+    m = Y_status_Map.shape[0]
+
+    dW = (X.T @ (Z_softMax - Y_status_Map)) / m
+    db = np.sum(Z_softMax - Y_status_Map, axis=0, keepdims=True).T / m
+
+    cache = {
+        "dW":dW,
+        "db":db
+    }
+    
+    return cache
+
+def gradient_Descent(parameters,cache,learning_rate=1.2):
+    
+    #dW and dB
+    dW = cache['dW']
+    db = cache['db']
+    
+    #w and b
+    W = parameters['W']
+    b = parameters['b']
+    
+    W = W - learning_rate * dW
+    b = b - learning_rate * db
+    
+    parameters = {
+        "W":W,
+        "b":b
+    }
+    return parameters
+    
+if __name__ == "__main__":
     main()

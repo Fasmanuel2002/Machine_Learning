4a00b246d99d3f5a8f1ae9846b0919c15c8d3836
Manuel Ruiz <manuelfasruiz@gmail.com>
2025-09-10 16:12:32 +0200

    Working in the Api

---
+++ Covid19ClassificationModel/xray_dataset_covid19/app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, UploadFile, File
+from pydantic import BaseModel
+from tensorflow.keras.model import load_model # type: ignore
+from PIL import Image
+from tensorflow.keras.preprocessing import image # type: ignore
+import io
+import cv2
+import numpy as np
+
+
+app = FastAPI()
+
+MODEL_IMAGE_PATH = "CovidDetector.h5"
+model = load_model(MODEL_IMAGE_PATH)
+
+
+@app.post("/CovidPredictor")
+async def uploadCovidPhoto(file : UploadFile = File(...)):
+    data = await file.read()
+
+    img = Image.open(io.BytesIO(data)).convert("RGB")
+    reshapedImage = cv2.resize(img, (64,64))
+    imageArray = image.img_to_array(reshapedImage)
+    imageArray = np.expand_dims(imageArray, axis=0)
+    img_array = imageArray / 255
+    prediction = model.predict(img_array)[0]
+     
+    return prediction
+

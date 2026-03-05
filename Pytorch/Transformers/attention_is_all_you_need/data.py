aa00918165327df6cbf2cd606982910f46eff37b
Fasmanuel2002 <112648218+Fasmanuel2002@users.noreply.github.com>
2026-03-05 21:08:52 +0100

    Working in the transformers

--- Pytorch/Transformers/attention_is_all_you_need/data.py
+++ Pytorch/Transformers/attention_is_all_you_need/data.py
@@ -1,21 +1,61 @@
-from typing import Tuple
+from typing import Tuple, List
 from torch.utils.data import random_split, DataLoader
 import torch
+from torchtext.data.functional import to_map_style_dataset
+from torch.utils.data import DataLoader
+from torchtext.vocab import build_vocab_from_iterator
+import torchtext.datasets as datasets
+import spacy
+import os
 
-def split_data_Train_Val_Test(batch_size : int = 32) -> Tuple[DataLoader, DataLoader, DataLoader]:
 
-
-    generator = torch.Generator().manual_seed(42)
-    
+def load_data() -> Tuple[List[str]]:
     with open("train.en", encoding="utf8") as f:
         english_data = f.read().splitlines()
 
     with open("train.de", encoding="utf8") as f:
         german_data = f.read().splitlines()
-
-    print(len(english_data), len(german_data))
     
     assert len(english_data) == len(german_data), "Dataset mismatch" 
+
+    return english_data, german_data
+
+
+def load_tokenizers() -> Tuple:
+    try:
+        spacy_german = spacy.load("de_core_news_sm")
+    except IOError:
+        os.system("python -m spacy download de_core_news_sm")
+        spacy_german = spacy.load("de_core_news_sm")
+    try:    
+        spacy_english = spacy.load("en_core_web_sm")
+    except IOError:
+        os.system("python -m spacy download en_core_web_sm")
+        spacy_english = spacy.load("en_core_web_sm")
+        
+    return spacy_english, spacy_german
+    
+
+def tokenize(text : str, tokinizer ) -> list:
+    return [tok.text for tok in tokinizer.tokenizer(text)]
+
+def yield_tokens(data_iter, tokinizer, index):
+    for from_to_tuple in data_iter:
+        yield tokinizer(from_to_tuple[index])
+        
+    
+def build_vocabulary(spacy_de , spacy_en):
+    
+    generator = torch.Generator().manual_seed(42)
+    
+    english_data, german_data = load_data()
+    
+    def tokenize_de(text):
+        return tokenize(text, spacy_de)
+    
+    def tokenize_en(text):
+        return tokenize(text, spacy_en)
+    
     
     dataset_pairs = list(zip(english_data, german_data))
     
@@ -30,23 +70,20 @@ def split_data_Train_Val_Test(batch_size : int = 32) -> Tuple[DataLoader, DataLo
     train_data, val_data, test_data = random_split(dataset=dataset_pairs,
                                                    lengths=[train_size, val_size, test_size],
                                                    generator=generator)
-    
-    train_loader = DataLoader(
-        dataset=train_data,
-        batch_size=batch_size,
-        shuffle=True
+    vocab_src = build_vocab_from_iterator(
+        yield_tokens(train_data , tokenize_de, index=0),
+        min_freq=2,
+        specials=["<s>", "</s>", "<blank>", "<unk>"]
     )
     
-    val_loader = DataLoader(
-        dataset=val_data,
-        batch_size=batch_size,
-        shuffle=False
+    vocab_tgt = build_vocab_from_iterator(
+        yield_tokens(train_data , tokenize_en, index=1),
+        min_freq=2,
+        specials=["<s>", "</s>", "<blank>", "<unk>"],
     )
     
-    test_loader = DataLoader(
-        dataset=test_data,
-        batch_size=batch_size,
-        shuffle=False
-    )
+    vocab_src.set_default_index(vocab_src["<unk>"])
+    vocab_tgt.set_default_index(vocab_tgt["<unk>"])
     
-    return train_loader, val_loader, test_loader
+    return vocab_src, vocab_tgt
+    

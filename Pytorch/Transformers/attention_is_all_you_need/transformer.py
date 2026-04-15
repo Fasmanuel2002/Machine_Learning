17507d76ad82a77bfd3a6d799d98a17cadd85946
Fasmanuel2002 <112648218+Fasmanuel2002@users.noreply.github.com>
2026-04-15 21:09:59 +0200

    Added the Positional Encoding from the model

---
+++ Pytorch/Transformers/attention_is_all_you_need/transformer.py
@@ -0,0 +1,22 @@
+import torch 
+import torch.nn as nn
+import math
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: int , max_len : int = 5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        
+        #Compute the positional encoding once in log space
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, : x.size(1)].requires_grad_(False)
+        return self.dropout(x)

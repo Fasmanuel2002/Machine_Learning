2ea015fc68d0d2a1639de8a0cbd6e0107f65cd5c
Fasmanuel2002 <112648218+Fasmanuel2002@users.noreply.github.com>
2026-04-20 18:46:08 +0200

    Added the seBERT model for training the fine tuning for two multi task heads

--- Pytorch/Transformers/seBERT/model/seBERT.py
+++ Pytorch/Transformers/seBERT/model/seBERT.py
@@ -6,79 +6,66 @@ from src.classic_ml.utils.compute_metrics import compute_metrics_multi_label
 import pandas as pd
 import numpy as np
 from typing import List
+from transformers import BertModel
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.model_selection import train_test_split
-from transformers import BertTokenizer, BertForSequenceClassification
-from transformers import TrainingArguments, Trainer
+import torch.nn as nn
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-
-class seBERT(BaseEstimator, ClassifierMixin):
-    def __init__(self, checkpoints_dir = "../checkpoints/", batch_size : int = 16):
-        self.trainer = None
-        self.checkpoints_dir = checkpoints_dir
-        self.model = BertForSequenceClassification.from_pretrained("../src/classic_ml/models/seBERT",num_labels=3, local_files_only=True).to(device)
-        self.tokenizer = BertTokenizer.from_pretrained("../src/classic_ml/models/seBERT",do_lower_case=True)
-        self.batch_size = batch_size
-        self.max_length = 128
-
-    def fit(self, X , y):
-        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
-        
-        X_train_tokens = self.tokenizer(X_train, padding=True, truncation=True, max_length=self.max_length)
-        
-        X_val_tokens = self.tokenizer(X_val, padding=True, truncation=True, max_length=self.max_length)
-
-        train_dataset = Dataset(X_train_tokens, y_train) 
-        val_dataset = Dataset(X_val_tokens, y_val)
-        
-        if not os.path.exists(self.checkpoints_dir):
-            os.makedirs(self.checkpoints_dir)
-
-        training_args = TrainingArguments(
-            output_dir=self.checkpoints_dir,
-            num_train_epochs=5,
-            per_device_train_batch_size=self.batch_size,
-            per_device_eval_batch_size=self.batch_size,
-            gradient_accumulation_steps=4,
-            eval_accumulation_steps=10,
-            evaluation_strategy="epoch",
-            save_strategy="epoch",
-            load_best_model_at_end=True
+class MLP(nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_channels, 64),
+            nn.ReLU(),
+            nn.Linear(64,32),
+            nn.ReLU(),
+            nn.Linear(32, output_channels)
         )
-        self.trainer = Trainer(
-            model=self.model,
-            args=training_args,
-            train_dataset=train_dataset,
-            eval_dataset=val_dataset,
-            compute_metrics=compute_metrics_multi_label
-        )
-        print(self.trainer.train())
-        return self
+
+    def forward(self, x):
+        return self.layers(x)
     
-    def predict_proba(self, X , y = None)  -> List[np.ndarray]:
-        y_probs = []
-        self.trainer.model.eval()
-        with torch.no_grad():
-            for _, X_row in enumerate(X):
-                inputs = self.tokenizer(X_row, padding =True, truncation = True, max_length = self.max_length, return_tensors = "pt").to(device)
-                outputs = self.trainer.model(**inputs)
-                probs = outputs[0].softmax(dim=1).cpu().detach().numpy()
-                y_probs.append(probs)
+
+class seBERT_MultiTask(nn.Module):
+    def __init__(self, encoder, dropout_prob=0.1):
+        super(seBERT_MultiTask, self).__init__()
+        self.encoder = encoder
+
+        self.hidden_size = self.encoder.config.hidden_size
         
-        return y_probs
-    def predict(self, X , y = None) -> List[np.ndarray]:
-        """Predict is evaluation of the model"""
-        y_probs = self.predict_proba(X, y=y)
-        y_pred = []
-        for y_prob in y_probs:
-            y_pred.append(y_prob.argmax())
+        self.dropout_prob = dropout_prob
+
+        self.dropout = nn.Dropout(self.dropout_prob)
+
+        self.head_worth_automating = nn.Linear(self.hidden_size, 1)
         
-        return y_pred
-    
-    def save_new_mode(self, path : str) -> None:
-        if not os.path.exists(path):
-            os.makedirs(path)
-        self.trainer.model.save_pretrained(path)
+        self.head_enough_information = nn.Linear(self.hidden_size, 1)
+
+        self.loss_fn = nn.BCEWithLogitsLoss()
+
+    def forward(self, input_ids, attention_mask = None, token_type_ids = None, labels_worth_automating = None, labels_enough_information = None):
+        output_encoder = self.encoder(input_ids = input_ids, attention_mask = attention_mask, token_type_ids = token_type_ids)
+        
+        # Representación del token [CLS]
+        cls_output  = output_encoder.last_hidden_state[:, 0, :] 
+
+        cls_output  = self.dropout(cls_output)
+
+        logits_worth_automating = self.head_worth_automating(cls_output).squeeze(-1)
+
+        logits_enough_information = self.head_enough_information(cls_output).squeeze(-1)
+
+        loss = None
+        if labels_worth_automating is not None and labels_enough_information is not None:
+            loss_worth_automating = self.loss_fn(logits_worth_automating, labels_worth_automating.float())
+            loss_enough_information = self.loss_fn(logits_enough_information, labels_enough_information.float())
+            loss = loss_worth_automating + loss_enough_information
+        
+        return {
+            "loss": loss,
+            "logits_worth_automating": logits_worth_automating,
+            "logits_enough_information": logits_enough_information
+        }

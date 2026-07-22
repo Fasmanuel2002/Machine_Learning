9843d2311276d32c5617c062d3793dab789defd8
ManuelFas <manuelfasruiz@gmail.com>
2026-07-22 11:19:48 -0400

    Added the Encoder module

--- Pytorch/Seq2Seq/models.py
+++ Pytorch/Seq2Seq/models.py
@@ -1,11 +1,35 @@
 from torch import nn
 
+
 class Encoder(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, input_dim : int , embedding_dim : int , hidden_dim : int, n_layers : int, dropout : int):
+        super().__init__()
+        """
+        input_dim is the size/dimensionality of the one-hot vectors that will be input to the encoder. This is equal to the input (source) vocabulary size.
+        embedding_dim is the dimensionality of the embedding layer. This layer converts the one-hot vectors into dense vectors with embedding_dim dimensions.
+        hidden_dim is the dimensionality of the hidden and cell states.
+        n_layers is the number of layers in the RNN.
+        dropout is the amount of dropout to use. This is a regularization parameter to prevent overfitting. Check out this for more details about dropout.
+        """
+        
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.embedding = nn.Embedding(num_embeddings=input_dim, embedding_dim=embedding_dim)
+        self.rnn_layers = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, num_layers=n_layers, dropout=dropout)
+        self.dropout = nn.Dropout(p=dropout)
     
-    def forward(self):
-        ...
+    
+    def forward(self, x):
+        # X = (src lenght, batch_size) -> (T, B)
+        x = self.dropout(self.embedding(x))
+        # X = embedding (src lenght , batch size, Hidden dim)-> (T, B, H)
+        outputs, (hidden, cell) = self.rnn_layers(x)
+        # outputs -> (T, B, H * n_directions)
+        # hidden -> (n_layers * n_directions, B, H)
+        # cell  -> (n_layers * n_directions, B, H)
+        # outputs are always from the top hidden layer
+        return hidden, cell
+        
 
 
 class Decoder(nn.Module):

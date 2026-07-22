402faacbf1898acb437f9641d01b07f712e99b05
ManuelFas <manuelfasruiz@gmail.com>
2026-07-21 21:09:03 -0400

    Created the Custom_colalte so all the sequence have the same lenght

--- Pytorch/Seq2Seq/dataset.py
+++ Pytorch/Seq2Seq/dataset.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 from typing import Tuple
 from torch.utils.data import random_split
+from torch.nn.utils.rnn import pad_sequence
 
 
 class BraindumpDataset(Dataset):
@@ -41,20 +42,37 @@ def split_dataset(dataset : BraindumpDataset, batch_size : int = 32) -> Tuple[Da
     train_loader = DataLoader(
     dataset=train_data,
     batch_size=batch_size,
-    shuffle=True
+    shuffle=True,
+    collate_fn=custom_collate # type: ignore
     )
     #VALIDATION SET
     validation_loader = DataLoader(
         dataset=val_data,
         batch_size=batch_size,
         shuffle=False,
+        collate_fn=custom_collate # type: ignore
     )
 
     #TEST SET
     test_loader = DataLoader(
         dataset=test_data,
         batch_size=batch_size,
-        shuffle=False
+        shuffle=False,
+        collate_fn=custom_collate # type: ignore
     )
     
     return train_loader, validation_loader, test_loader
+
+
+def custom_collate(data : BraindumpDataset):
+    
+    inputs, targets = zip(*data)
+    
+    inputs_padded = pad_sequence(inputs, batch_first=True, padding_value=0) # type: ignore
+    targets_padded = pad_sequence(targets, batch_first=True, padding_value=0) # type: ignore
+    
+    return { #(6)
+        'tokenized_input': inputs_padded,
+        'label': targets_padded
+    }
+    
